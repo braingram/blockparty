3baf36cfe5e9c653555a085e2bdd7a1839a143d5
braingram <brettgraham@gmail.com>
2018-04-13 11:18:02 -0400

    wip pandas rewrite, next, need sequences

--- process_directory.py
+++ process_directory.py
@@ -47,12 +47,14 @@ if os.path.exists(afn):
     rfid_df = rfid_df[rfid_df['data0'].isin(animals['rfid'])]
 else:
     # necessary columns 'rfid'
+    tags = rfid_df['data0'].unique()
     animals = pandas.DataFrame({
-        'rfid': rfid_df['data0'].unique(),
+        'rfid': tags,
+        'name': tags,
     })
 
 
-# add datetime column
+# add datetime column, TODO do this after merging etc...
 fets = numpy.array(fets)
 slope, intercept = numpy.polyfit(fets[:, 1], fets[:, 0], 1)
 rfid_df['datetime'] = pandas.to_datetime(
@@ -61,6 +63,9 @@ rfid_df['datetime'] = pandas.to_datetime(
         ) * 1000).astype('int64'),
     unit='ms')
 
+# add name column, TODO do this after merging etc...
+rfid_df['name'] = rfid_df['data0'].replace(
+    {i.rfid: i.name for i in animals.itertuples()})
 
 # TODO merge close rfid reads    
 def find_shorted_board_to_board_time(df):
@@ -70,7 +75,7 @@ def find_shorted_board_to_board_time(df):
     return (t[1:][m] - t[:-1][m]).min()
 
 
-grouped_rfid_df = rfid_df.group_by('data0')
+grouped_rfid_df = rfid_df.groupby('data0')
 rfid_merge_threshold = grouped_rfid_df.apply(
     find_shorted_board_to_board_time).min()
 
@@ -79,7 +84,7 @@ for aid in grouped_rfid_df.groups:
     df = grouped_rfid_df.get_group(aid)
     # keep all events where dt > thresh or board changed
     t = numpy.array(df['time'])
-    b = numpy.array(df['board']
+    b = numpy.array(df['board'])
     m = numpy.ones_like(t, dtype='bool')
     m[1:] = (t[1:] - t[:-1]) > rfid_merge_threshold
     m[1:] |= b[1:] != b[:-1]
@@ -88,9 +93,14 @@ for aid in grouped_rfid_df.groups:
     
     # TODO convert from reads to sequence to occupancy
     b = numpy.array(ad[aid]['board'])
+    
+    # find all 'starts' (where board at t0 is 1 off from board at t1)
+    # for each start, trace out until next start (or error)
+    # if the next start agrees, accept, if not, reject
+    # compute reliability score (accepts vs rejects)
+    # output occupancy (start time, end time, cage, animal[name], ?)
+    # what about unknown period? fill in possible cages?
 
-# TODO occupancy from tube sequence
-# TODO merge occupancies from all animals
 # TODO find multi-animal events
 # TODO generate chase matrix
 # TODO save, plot, print
@@ -102,5 +112,5 @@ print("Duration of experiment: %s" % rfid_df['datetime'].ptp())
 print()
 ec = rfid_df['data1'].value_counts()
 print("RFID read errors: %s" % (ec.get('1', '0')))
-print("Animal tags and reads:")
-print(rfid_df['data0'].value_counts())
+print("Raw animal tags and reads:")
+print(rfid_df['name'].value_counts())
