fe5405734ffe46e057f295017710ee225466bef5
braingram <brettgraham@gmail.com>
2019-02-04 10:36:45 -0500

    update wedge plot to use newer bokeh with png export and fixing alpha compositing

--- wedge_plot.py
+++ wedge_plot.py
@@ -7,6 +7,7 @@ from six.moves import cStringIO as StringIO
 
 import bokeh.palettes
 from bokeh.plotting import figure, show, output_file
+from bokeh.io import export_png
 
 # read in reads
 reads = []
@@ -31,10 +32,11 @@ animal_colors = bokeh.palettes.viridis(len(animals))
 max_reads_per_tube = max([max(t.values()) for t in tubes.values()])
 
 
-width = 800
-height = 800
-inner_radius = 90
-outer_radius = 300 - 10
+res = 3
+width = 800 * res
+height = 800 * res
+inner_radius = 90 * res
+outer_radius = (300 - 10) * res
 
 minr = 0
 maxr = numpy.ceil(max_reads_per_tube / 1000.) * 1000.
@@ -50,7 +52,8 @@ small_angle = big_angle / (len(animals) + 1)
 
 p = figure(plot_width=width, plot_height=height, title="",
     x_axis_type=None, y_axis_type=None,
-    x_range=(-420, 420), y_range=(-420, 420),
+    x_range=(-420 * res, 420 * res),
+    y_range=(-420 * res, 420 * res),
     min_border=0, outline_line_color="black",
     background_fill_color="#ffffff")
 
@@ -72,7 +75,8 @@ labels = numpy.linspace(0, maxr, nlines)
 radii = a * labels + b
 p.circle(0, 0, radius=radii, fill_color=None, line_color="#c8a48c")
 p.text(0, radii, [str(int(r)) for r in labels],
-       text_font_size="10pt", text_align="center", text_baseline="middle")
+       text_font_size=(str(10 * res) + "pt"),
+       text_align="center", text_baseline="bottom")
 #p.text(0, [radii[-1], ], [str(int(labels[-1])), ],
 #       text_font_size="8pt", text_align="center", text_baseline="middle")
 
@@ -91,25 +95,54 @@ p.annular_wedge(0, 0, inner_radius-10, outer_radius+10,
 
 # tube labels
 ts = numpy.arange(0, len(tubes), dtype='f8') / len(tubes) * numpy.pi * 2.
-r = inner_radius - 12
+r = inner_radius - 12 * res
 txs = numpy.sin(ts) * r
 tys = numpy.cos(ts) * r
 p.text(
     txs, tys, [str(i) for i in range(len(tubes))],
-    text_font_size="9pt", text_align="center", text_baseline="middle")
+    text_font_size=(str(9 * res) + "pt"),
+    text_align="center", text_baseline="middle")
 
 # draw out path for each animal
-radius = inner_radius - 20
+# pre-process to count number of lines between X and Y
+# then plot out X to Y but set alpha to count / maxN
+
+radius = inner_radius - 20 * res
 for (aid_i, aid) in enumerate(animals):
-    ts = numpy.array([r[2] for r in reads if r[1] == aid], dtype='f8')
+    ts = numpy.array([r[2] for r in reads if r[1] == aid])
+    segments = {}
+    for pn in zip(ts[:-1], ts[1:]):
+        segments[pn] = segments.get(pn, 0) + 1
+    max_n = max(segments.values())
+    """
     ts /= len(tubes)
     ts *= numpy.pi * 2.
     # convert tubes to xys
-    r = radius - aid_i * 2
+    """
+    r = radius - aid_i * 2 * res
+    """
     xs = numpy.sin(ts) * r
     ys = numpy.cos(ts) * r
-    p.line(xs, ys, color=animal_colors[aid_i], line_width=1, line_alpha=0.3)
-                
-output_file("reads_per_tube.html", title="blockparty reads per tube")
+    """
+    tox = lambda i: numpy.sin(i / 10. * numpy.pi * 2.) * r
+    toy = lambda i: numpy.cos(i / 10. * numpy.pi * 2.) * r
+    xys = [[], []]
+    alphas = []
+    for pn in segments:
+        segments[pn] /= float(max_n)
+        px, nx = tox(pn[0]), tox(pn[1])
+        py, ny = toy(pn[0]), toy(pn[1])
+        xys[0].append([px, nx])
+        xys[1].append([py, ny])
+        alphas.append(segments[pn])
+    # print(segments)
+    #p.line(xs, ys, color=animal_colors[aid_i], line_width=1, line_alpha=0.3)
+    p.multi_line(xys[0], xys[1], color=animal_colors[aid_i], line_width=res, alpha=alphas)
 
-show(p)
+#p.output_backend = "svg"
+#export_svgs(p, filename='plot.svg')
+
+export_png(p, filename="reads_per_tube.png")
+
+#output_file("reads_per_tube.html", title="blockparty reads per tube")
+#show(p)
